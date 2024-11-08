58138d55c6970aaa1fea0da6b9064d272082cb0c
Jerry Jiang <jerry.jiang@discordapp.com>
2024-11-08 15:27:30 -0800

    Scrolling bug.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,39 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import * as React from "react";
+import { ScrollView, View } from "react-native";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+function useGesture() {
+  return React.useMemo(() => {
+    return (
+      Gesture.Pan()
+        // All of the following have no effect on the bug.
+        // .enabled(false)
+        // .hitSlop({ bottom: -100 })
+        // .onTouchesDown((_, manager) => manager.fail())
+        .manualActivation(true)
+    );
+  }, []);
+}
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
+export default function HomeScreen() {
+  const gesture = useGesture();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <GestureHandlerRootView>
+      <GestureDetector gesture={gesture}>
+        <ScrollView>
+          <View
+            style={{ width: "100%", height: 300, backgroundColor: "red" }}
+          />
+          <View
+            style={{ width: "100%", height: 300, backgroundColor: "blue" }}
+          />
+        </ScrollView>
+      </GestureDetector>
+    </GestureHandlerRootView>
   );
 }
