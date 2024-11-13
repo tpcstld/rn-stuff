173ec2cb206ad95b1e3a73e8f231d820ceaae066
Jerry Jiang <jerry.jiang@discordapp.com>
2024-11-12 16:33:33 -0800

    Talkback scrolling repro.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,39 @@
-import { View } from "react-native";
+import React from "react";
+import {
+  ScrollView as ScrollViewRN,
+  Pressable,
+  StyleSheet,
+} from "react-native";
+import {
+  GestureHandlerRootView,
+  ScrollView,
+} from "react-native-gesture-handler";
 
 export default function RootLayout() {
+  const ScrollViewComponent = false ? ScrollView : ScrollViewRN;
   return (
-    <View style={{ height: 100, width: "100%", backgroundColor: "red" }} />
+    <GestureHandlerRootView
+      style={[StyleSheet.absoluteFill, { backgroundColor: "red" }]}
+    >
+      <ScrollViewComponent>
+        {[
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24,
+        ].map((item) => (
+          <Pressable
+            key={item}
+            style={{
+              height: 100,
+              backgroundColor: `#${Math.floor(
+                Math.random() * 16777215
+              ).toString(16)}`,
+            }}
+            onPress={() => {
+              console.log("pressed");
+            }}
+          />
+        ))}
+      </ScrollViewComponent>
+    </GestureHandlerRootView>
   );
 }
